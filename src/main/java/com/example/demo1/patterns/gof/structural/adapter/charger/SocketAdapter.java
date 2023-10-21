45da48057cfdb5c97c3b6293d4fd77ffc6337460
Yevhen Bufan <bufan7140911@gmail.com>
2023-10-21 22:20:08 +0300

    Added adapter javadoc

--- src/main/java/com/example/demo1/patterns/gof/structural/adapter/charger/SocketAdapter.java
+++ src/main/java/com/example/demo1/patterns/gof/structural/adapter/charger/SocketAdapter.java
@@ -1,5 +1,10 @@
 package com.example.demo1.patterns.gof.structural.adapter.charger;
 
+/**
+ * Adapter is a structural design pattern that allows objects with incompatible interfaces to collaborate.
+ *  Use the Adapter class when you want to use some existing class, but its interface isn’t compatible with the rest of your code.
+ *  Use the pattern when you want to reuse several existing subclasses that lack some common functionality that can’t be added to the superclass.
+ */
 public interface SocketAdapter {
 
     public Volt get120Volt();
