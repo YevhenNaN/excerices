e1bb6c2fa54520c50d304864b7c0a0cec0735366
Yevhen Bufan <bufan7140911@gmail.com>
2023-10-25 23:02:38 +0300

    Refactor main

--- src/main/java/com/example/demo1/Demo1Application.java
+++ src/main/java/com/example/demo1/Demo1Application.java
@@ -31,6 +31,35 @@ public class Demo1Application {
         SpringApplication.run(Demo1Application.class, args);
         StreamOperationsService streamOperationsService = new StreamOperationsService();
         streamOperationsService.performStreamOperations();
+        creationPatternExamples();
+
+        // Adapter
+        // Round fits round, no surprise.
+        RoundHole hole = new RoundHole(5);
+        RoundPeg rpeg = new RoundPeg(5);
+        if (hole.fits(rpeg)) {
+            System.out.println("Round peg r5 fits round hole r5.");
+        }
+
+        SquarePeg smallSqPeg = new SquarePeg(2);
+        SquarePeg largeSqPeg = new SquarePeg(20);
+        // hole.fits(smallSqPeg); // Won't compile.
+
+        // Adapter solves the problem.
+        SquarePegAdapter smallSqPegAdapter = new SquarePegAdapter(smallSqPeg);
+        SquarePegAdapter largeSqPegAdapter = new SquarePegAdapter(largeSqPeg);
+        if (hole.fits(smallSqPegAdapter)) {
+            System.out.println("Square peg w2 fits round hole r5.");
+        }
+        if (!hole.fits(largeSqPegAdapter)) {
+            System.out.println("Square peg w20 does not fit into round hole r5.");
+        }
+
+        testClassAdapter();
+        testObjectAdapter();
+    }
+
+    private static void creationPatternExamples() {
         //abstract factory
         new AnimalAbstractFactory().createAnimal(AnimalType.SKY, AnimalEra.CENOZOIC);
         //Factory method
@@ -74,31 +103,6 @@ public class Demo1Application {
         System.out.println("emps List: " + emps.getEmployeeList());
         System.out.println("empsNew List: " + list);
         System.out.println("empsNew1 List: " + list1);
-
-        // Adapter
-        // Round fits round, no surprise.
-        RoundHole hole = new RoundHole(5);
-        RoundPeg rpeg = new RoundPeg(5);
-        if (hole.fits(rpeg)) {
-            System.out.println("Round peg r5 fits round hole r5.");
-        }
-
-        SquarePeg smallSqPeg = new SquarePeg(2);
-        SquarePeg largeSqPeg = new SquarePeg(20);
-        // hole.fits(smallSqPeg); // Won't compile.
-
-        // Adapter solves the problem.
-        SquarePegAdapter smallSqPegAdapter = new SquarePegAdapter(smallSqPeg);
-        SquarePegAdapter largeSqPegAdapter = new SquarePegAdapter(largeSqPeg);
-        if (hole.fits(smallSqPegAdapter)) {
-            System.out.println("Square peg w2 fits round hole r5.");
-        }
-        if (!hole.fits(largeSqPegAdapter)) {
-            System.out.println("Square peg w20 does not fit into round hole r5.");
-        }
-
-        testClassAdapter();
-        testObjectAdapter();
     }
 
     private static void testObjectAdapter() {
