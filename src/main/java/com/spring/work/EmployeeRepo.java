0ebc02efa271badbbefadf0605f1aaba4b653b88
Yevhen Bufan <bufan7140911@gmail.com>
2023-11-13 20:30:06 +0200

    delete unused code

--- src/main/java/com/spring/work/EmployeeRepo.java
+++
@@ -1,8 +0,0 @@
-//package com.spring.work;
-//
-//import org.springframework.data.jpa.repository.JpaRepository;
-//import org.springframework.stereotype.Repository;
-//
-//@Repository
-//public interface EmployeeRepo extends JpaRepository<Employee, Long> {
-//}
--- src/main/java/com/spring/work/ResourceStatus.java
+++
@@ -1,4 +0,0 @@
-package com.spring.work;
-
-public enum ResourceStatus {
-}
